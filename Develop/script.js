b3c0c9d9e59ee615e23fc85c9478adc6bdf0e6d1
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-03 13:04:12 -0500

    saveData function and for each

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,92 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
+var textAreas = $('.description')
+var timeContainer = $('.time-container')
+var timeArea = $('.time-area')
 
-$( document ).ready(function() {
+$(document).ready(function () {
     $("#currentDay").text(currentDate);
 });
 
-// add event listeners to individual buttons
-// array
+var timeSlots = [
+    "9",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+];
+
+timeSlots.forEach(function (timeSlot) {
+    var timeListContainer = document.createElement('div')
+    var timeList = document.createElement('div')
+    var timeNumber = document.createElement('p')
+    var inputContainer = document.createElement('div')
+    var input = document.createElement('textarea')
+    var saveBtnContainer = document.createElement('div')
+    var saveBtn = document.createElement('button')
+    input.setAttribute("id", timeSlot)
+    input.setAttribute("class", "description")
+
+    if (timeSlot >= 9) {
+        timeNumber.textContent = timeSlot + 'am'
+    } else {
+        timeNumber.textContent = timeSlot + 'pm'
+    }
+
+    if (timeSlot === 12) {
+        timeNumber.textContent = timeSlot + 'pm'
+    }
+
+    saveBtn.textContent = 'SAVE'
+    timeListContainer.append(timeList)
+    timeList.append(timeNumber)
+    inputContainer.append(input)
+    saveBtnContainer.append(saveBtn)
+
+    timeNumber.setAttribute('data-time', timeSlot)
+    saveBtn.setAttribute('class', 'saveBtn')
+
+    timeArea.append(timeListContainer, inputContainer, saveBtnContainer)
+    timeContainer.append(timeArea)
+});
+
 var saveBtns = $(".saveBtn");
 console.log(saveBtns)
 
-saveBtns.each(function( index ) {
-    $(saveBtns[index]).on("click", function() {
-        // alert("button Clicked with index# " + index)
-        localStorage.textAreas(index)
+saveBtns.each(function (index) {
+    $(saveBtns[index]).on("click", function () {
+        alert("button Clicked with index# " + index)
+        saveData();
     })
 });
-//sibling
-var textAreas = $(".description");
-console.log(textAreas)
+
+function saveData(){
+        var time = $('.hour').val();
+        var value = $('description').val();
+        localStorage.setItem(time, value);
+};
+    
+
+  
 
 
 
+
+
+
+
+
+
+
+
+
+// $('#test').html("Test");
+// localStorage.content = $('#test').html();
+// $('#test').html(localStorage.content);
+// })
 
 // try rewriting this code to fit your array needs
 // localStorage only supports strings. Use JSON.stringify() and JSON.parse().
