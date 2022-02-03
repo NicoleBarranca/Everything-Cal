676c28e7515f686cd9b1bed7b4b84f059fb1a6ef
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-02 23:15:33 -0500

    create array

--- Develop/script.js
+++ Develop/script.js
@@ -4,5 +4,53 @@ $( document ).ready(function() {
     $("#currentDay").text(currentDate);
 });
 
+// add event listeners to individual buttons
+// array
+var saveBtns = $(".saveBtn");
+console.log(saveBtns)
+
+saveBtns.each(function( index ) {
+    $(saveBtns[index]).on("click", function() {
+        alert("button Clicked with index# " + index)
+    })
+});
+//sibling
+var textAreas = $(".description");
+console.log(textAreas)
+
+// try rewriting this code to fit your array needs
+// localStorage only supports strings. Use JSON.stringify() and JSON.parse().
+
+// var names = [];
+// names[0] = prompt("New member name?");
+// localStorage.setItem("names", JSON.stringify(names));
+
+// //...
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+
+
+
+
+
+
+
+// var animals = ["dog", "cat", "monkey", "beaver"]
+
+// $(animals).each(function(i) {
+//     console.log(animals[i])
+// })
+
+// for(var i = 0; i < animals.length; i++) {
+//     console.log(animals[i])
+}
+
+// associate buttons with appropriate text areas
+
+// based on the indivduls button we want to save appropriate text area value to local storage
+
+// make sure all text areas have correct background color
+
+
+
 
 // document.getElementById('currentDay').innerHTML = currentDate;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Everything-Cal
+
+<img src="./images/sc.jpg" width="400px" height="250px">
