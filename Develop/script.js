fc57bc10146e505f47d5512fc66eb61b5bce2fe3
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-05 11:05:48 -0500

    add comments to css, js, html

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
+    // Current Date for top of the web page
     var currentDate = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").text(currentDate);
-    
+    // variable for the current hour, later in the .each we can define past, present and future for color coding
     var currentHour = moment().format("H");
 
 
- 
+    // Save content in text areas to local storage
     $(".saveBtn").on("click", function () {
         var value = $(this).prev().val();
         var key = $(this).parent().attr('id');
         console.log(key, value)
         localStorage.setItem(key, value);
-     
+
     }
     )
-
-    $(".description").each(function(){
-        if (Number($(this).attr('id')) < Number(currentHour)){
+    // if the current hour is greater than the hour in this time block give it the class of past
+    // else if the current hour is equal to this time block give it the class of present
+    // else (not greater and not equal to) give time block class of future
+    $(".description").each(function () {
+        if (Number($(this).attr('id')) < Number(currentHour)) {
             $(this).addClass("past")
-        } else if (Number($(this).attr('id')) === Number(currentHour)){
-                $(this).addClass("present")
+        } else if (Number($(this).attr('id')) === Number(currentHour)) {
+            $(this).addClass("present")
         } else $(this).addClass("future")
     })
-    
 
-        $(".time-block").each(function(){
-            var key = $(this).attr('id');
-            var value = localStorage.getItem(key);
-            $(this).children(".description").val(value);
-        })
+    // In the time block with class of .time-block, we want to save text in text area with the class of .description
+    $(".time-block").each(function () {
+        var key = $(this).attr('id');
+        var value = localStorage.getItem(key);
+        $(this).children(".description").val(value);
+    })
 });
